95cf68047f09ac61f92138682e5862474bed55d1
maria eduarda <mariaeduardalivros@gmail.com>
2025-10-11 16:22:27 -0300

    Implementação da função lança_dados + caso de teste

--- src/Model/Dado.java
+++ src/Model/Dado.java
@@ -2,4 +2,14 @@ package Model;
 
 public class Dado {
 
+    Random random = new Random();
+	
+	//retorna o valor do lançamento dos dois dados
+	public int lanca_dados() 
+	{
+		int dado_um = random.nextInt(1,6);
+		int dado_dois = random.nextInt(1,6);
+		return dado_um + dado_dois;
+	}
+
 }
--- test/Model/DadoTest.java
+++ test/Model/DadoTest.java
@@ -1,5 +1,17 @@
 package Model;
+import org.junit.*;
+import static org.junit.Assert.*;
 
 public class DadoTest {
-
+	
+	@Test (timeout = 5000)
+	public void testaLancamentoDados() {
+		Dado d = new Dado();
+		int resultado_dados = d.lanca_dados();
+		boolean expressao = resultado_dados >= 2 && resultado_dados <= 12;
+		assertTrue("Resultado Inválido! A soma dos dados não estava entre 2 e 12!", expressao);
+		
+		
+	}
+	
 }
