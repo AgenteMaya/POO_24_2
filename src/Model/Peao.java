a0499c00bbc4f06651ceb2baded47fd38fb9814c
maria eduarda <mariaeduardalivros@gmail.com>
2025-10-11 18:33:16 -0300

    iniciando a implementação da classe Peao e PeaoTest

--- src/Model/Peao.java
+++ src/Model/Peao.java
@@ -1,5 +1,22 @@
 package Model;
 
 class Peao {
-
+	int id;
+	int dinheiro;
+	int posicao;
+	
+	public Peao(int id_peao) {
+		id = id_peao;
+	} 
+		
+	public void setaPosicaoPeao(int pos) {
+		posicao = pos;
+		System.out.printf("Jogador %d está na posição %d do tabuleiro\n", id, posicao);
+		return;
+	}
+	
+	public int pegaPosicaoPeao(int id_peao) {
+		return posicao;
+	}
+	
 }
--- test/Model/PeaoTest.java
+++ test/Model/PeaoTest.java
@@ -1,5 +1,19 @@
 package Model;
+import org.junit.*;
+import static org.junit.Assert.*;
 
 public class PeaoTest {
-
+	
+	@Test (timeout=5000)
+	public void testaPosicionaPeao() {
+		int peaoDaVez = 1;
+		int deslocamento = 6; //mocking de um possível valor retornado pela lanca_dados
+		
+		Peao peao = new Peao(peaoDaVez);
+		peao.setaPosicaoPeao(deslocamento);
+		int pos = peao.pegaPosicaoPeao(peaoDaVez);
+			
+		assertEquals("A posição setada não é a mesma do lançamento dos dados",
+				pos, deslocamento);
+	}
 }
